a7ef18048dcd16b4b1d0d2fc3c7fab0899c69ed1
itsLikith <saragadamlikith48@gmail.com>
2025-05-19 11:59:20 +0530

    configured tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRouter from './routes/routes.jsx';
 function App() {
   return (
     <>
-      <h1>See you at the movies</h1>
+      <h1 className="text-3xl font-bold">See you at the movies</h1>
     </>
   );
 }
