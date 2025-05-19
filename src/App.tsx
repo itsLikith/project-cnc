a95a52b02ca5c421d0dc51bf894f39138585202c
itsLikith <saragadamlikith48@gmail.com>
2025-05-19 13:32:47 +0530

    changed configurations

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <Button variant="outline">See you at the movies</Button>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
